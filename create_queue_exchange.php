e5fbf1463af2d4cd901c60a45fd64a552bc16bd3
Gerardo Herrera <gerardo.herrera@funiber.org>
2024-04-02 18:11:02 -0500

    add example with retries queue

---
+++ create_queue_exchange.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+$exchangeName = "saggitarius-a";
+$exchangeTopic = "saggitarius-a-retry";
+$exchangeDeadLetter = "saggitarius-a-dead-letter";
+
+$queues = [
+    'sales.applicant_management',
+    'financial.online_payments',
+];
+
+
+$connection = new AMQPStreamConnection('rabbit-mq-test-php_rabbitmq_1', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+$channel->exchange_declare($exchangeName,'fanout',false, true);
+$channel->exchange_declare($exchangeTopic,'topic',false, true);
+$channel->exchange_declare($exchangeDeadLetter, 'topic',false, true);
+
+foreach($queues as $queue){
+
+    $channel->queue_declare($queue, false, true);
+    $channel->queue_bind($queue, $exchangeName);
+
+}
+
+foreach($queues as $queue){
+
+    $channel->queue_declare($queue . ".retry" , false, true, false, true, false, [
+        'x-message-ttl' => ['I', 3000],
+        'x-dead-letter-exchange' => ['S', $exchangeName],
+    ]);
+
+    $channel->queue_bind($queue . ".retry", $exchangeTopic);
+
+}
+
+foreach($queues as $queue){
+
+    $channel->queue_declare($queue . ".dead_letter" , false, true);
+
+    $channel->queue_bind($queue . ".dead_letter", $exchangeDeadLetter);
+
+}
--- send.php
+++ send.php
@@ -5,13 +5,11 @@ require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
-$connection = new AMQPStreamConnection('rabbittest_rabbitmq_1', 5672, 'guest', 'guest');
+$connection = new AMQPStreamConnection('rabbit-mq-test-php_rabbitmq_1', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
-$channel->queue_declare('hello', false, true, false, false);
-
 $msg = new AMQPMessage('Hello Worldasdsadsad!');
-$channel->basic_publish($msg, '', 'hello');
+$channel->basic_publish($msg, "saggitarius-a");
 
 echo " [x] Sent 'Hello World!'\n";
 
